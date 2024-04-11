2471d9e2aae5cbd10fea04b22f7bf829460b45b0
Lucas Galhardo Lima <lima.galhardo@gmail.com>
2024-04-10 21:11:53 -0300

    started testing layout

---
+++ layouts/default.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import { SpeedInsights } from "@vercel/speed-insights/vue";
+</script>
+
+
+<template>
+    <div>
+        <SpeedInsights />
+        <AppHeader />
+        <slot />
+        <AppFooter />
+    </div>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,11 @@
+<template>
+    <div>
+        <h1>Welcome to the homepage</h1>
+        <AppAlert>
+            This is an auto-imported component
+        </AppAlert>
+
+        <UButton color="primary" variant="link">Button</UButton>
+        <UButton color="primary" variant="solid">Button</UButton>
+    </div>
+</template>
